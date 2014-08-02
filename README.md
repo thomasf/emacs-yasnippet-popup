cfb37d87471ca174b19c75d726537a197bb55229
Thomas Frössman <thomasf@jossystem.se>
2014-08-02 21:54:10 +0200

    Add initial files

---
+++ README.md
@@ -0,0 +1,26 @@
+# yasnippet-popup
+
+## Summary
+
+YAsnippet prompt using popup.el
+
+## Installing
+
+You will need Emacs 24+, `make` and [Cask](https://github.com/cask/cask) to
+build the project.
+
+    cd yasnippet-popup
+    make && make install
+
+
+## Contributing
+
+Yes, please do! See [CONTRIBUTING][] for guidelines.
+
+## License
+
+See [COPYING][]. Copyright (c) 2014 Thomas Frössman.
+
+
+[CONTRIBUTING]: ./CONTRIBUTING.md
+[COPYING]: ./COPYING
---
+++ yasnippet-popup.el
@@ -0,0 +1,33 @@
+;;; yasnippet-popup.el --- YAsnippet prompt using popup.el
+
+;; Copyright (C) 2014 Thomas Frössman
+
+;; Author: Thomas Frössman <thomasf@jossystem.se>
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; YAsnippet prompt using popup.el
+
+;;; Code:
+
+
+
+(provide 'yasnippet-popup)
+
+;;; yasnippet-popup.el ends here
